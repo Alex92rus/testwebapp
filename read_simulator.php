1e8026bd2f9666c6304fd76cd17e2234055d1f86
Alex92rus <aleksandar.rusinov@gmail.com>
2014-12-03 11:51:32 +0000

    Sample input simulator

---
+++ read_simulator.php
@@ -0,0 +1,32 @@
+<?php
+$url = 'qualoccupy.azurewebsites.net/add_data.php';
+$handle = fopen("inputfile.csv", "r");
+if ($handle) {
+    while (($data = fgetcsv($handle)) !== false) {
+	
+		$fields = array(
+								'DoorID' => urlencode($data[0]),
+								'transition' => urlencode($data[1]),
+								'Confidence' => urlencode($data[2])
+						);
+
+		$fields_string =  http_build_query( $fields ) ;
+
+		//open connection
+		$ch = curl_init();
+
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+		echo $fields_string;
+		//execute post
+		$result = curl_exec($ch);	
+		curl_close($ch);	// free url resources (ch also). Could slow down execution to emulate reality
+	
+    }
+} else {
+    // error opening the file.
+} 
+fclose($handle);
+?>
