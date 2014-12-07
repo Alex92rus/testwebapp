32ac9fc1ccf14d31edc41683d752951e28193a9d
Alex92rus <aleksandar.rusinov@gmail.com>
2014-12-07 21:20:11 +0000

    Improved simulator

--- read_simulator.php
+++ read_simulator.php
@@ -3,27 +3,29 @@ $url = 'http://qualoccupy.azurewebsites.net/add_data_simulator.php';
 $handle = fopen("inputfile.csv", "r");
 if ($handle) {
     while (($data = fgetcsv($handle)) !== false) {
-	
-		$fields = array(
-								'DoorID' => urlencode($data[0]),
-								'transition' => urlencode($data[1]),
-								'Confidence' => urlencode($data[2])
-						);
+		
+			$fields = array(
+									'DoorID' => urlencode($data[0]),
+									'transition' => urlencode($data[1]),
+									'Confidence' => urlencode($data[2])
+							);
 
-		$fields_string =  http_build_query( $fields ) ;
+			$fields_string =  http_build_query( $fields ) ;
 
-		//open connection
-		$ch = curl_init();
+			//open connection
+			$ch = curl_init();
 
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch,CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-		echo $fields_string;
-		//execute post
-		$result = curl_exec($ch);	
-		curl_close($ch);	// free url resources (ch also). Could slow down execution to emulate reality
-	
+			//set the url, number of POST vars, POST data
+			curl_setopt($ch,CURLOPT_URL,$url);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+			echo $fields_string;
+			//execute post
+			$result = curl_exec($ch);	
+			curl_close($ch);	// free url resources (ch also). Could slow down execution to emulate reality
+			
+			//Wait between 0 and 5 seconds
+			sleep( rand(0, 5));
     }
 } else {
     // error opening the file.
